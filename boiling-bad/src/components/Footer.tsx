2b565990ead471a8f81fe1caba7d0866a2ae2f7b
Shreya Nakum <shreyanakum2@gmail.com>
2025-02-01 17:14:14 -0800

    stuff

--- boiling-bad/src/components/Footer.tsx
+++ boiling-bad/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2023 Boiling Bad. All rights reserved.</p>
+      <p>&copy; 2025 Boiling Bad. All rights reserved.</p>
     </footer>
   )
 }
