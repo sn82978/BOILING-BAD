785fa8b0b9f20adfc172b3a0865d478ccedaae9b
Shreya Nakum <shreyanakum2@gmail.com>
2025-03-23 21:56:21 -0700

    about us update plus image fix attempt

--- boiling-bad/src/pages/AboutUs.tsx
+++ boiling-bad/src/pages/AboutUs.tsx
@@ -6,8 +6,8 @@ function AboutUs() {
       <h1>join us</h1>
       <p>free will to boil.</p>
       <p>we are two uci students and we met because our moms met on a whatsapp group and said we should be roommates</p>
-      <img src="/BOILING-BAD/public/images/IMG_9786.JPG" alt="lil cros" className={styles.responsiveImage} />
-      <p>you should give us money at our venmos @Shreya-Nakum and @ 👅</p>
+      <img src="IMG_9786.JPG" alt="lil cros" className={styles.responsiveImage} />
+      <p>fund us! we are @Shreya-Nakum and @tejasvini_ramesh 👅</p>
     </div>
   )
 }
