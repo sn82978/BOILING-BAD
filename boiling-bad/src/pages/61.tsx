f7d414e2198156253d75b84d825fa01b0a9d7035
Shreya Nakum <shreyanakum2@gmail.com>
2025-12-28 14:54:22 -0800

    bb name issue

--- boiling-bad/src/pages/61.tsx
+++ boiling-bad/src/pages/61.tsx
@@ -6,8 +6,8 @@ function F25Content() {
             <h1>F25</h1>
             <p>published: december 28, 2025</p>
             <h2>bideo</h2>
-            <video width="640" height="360" controls>
-                <source src="/BOILING-BAD/videos/bottombun2_fixed.mp4" type="video/mp4" />
+            <video width="640" height="360" controls> 
+                <source src="/BOILING-BAD/videos/bottumbun2_fixed.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <p>shoutout to beach for existing</p>
