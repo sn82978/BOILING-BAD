3bd4c6829d291e04933f56b85465f45dfc77febe
Shreya Nakum <shreyanakum2@gmail.com>
2026-03-07 00:10:37 -0600

    finished octane review

--- boiling-bad/src/pages/72.tsx
+++ boiling-bad/src/pages/72.tsx
@@ -28,9 +28,32 @@ function DonnyBoyContent() {
                     <p>this song isn't really his vibe he's tryna sounds like d4vd</p>
                     <li>"Rosary (ft. Travis Scott)": 7.0/10</li>
                     <p>beat was very basic but travis feature was fire</p>
-                    <li>"All The Signs (feat. Teezo Touchdown)": /10</li>
+                    <li>"All The Signs (feat. Teezo Touchdown)": 8.5/10</li>
                     <p>the beginning itches our brain</p>
                     <p>his singing is good on this one</p>
+                    <li>"Call Back": 4.7/10</li>
+                    <p>he's for sure singing from the nose here, quite nasally we say</p>
+                    <li>"Tuition": 6.0/10</li>
+                    <p>very chill, not crazy, but nothing really stands out to us</p>
+                    <li>"K9 (feat. SahBabii)": 7.9/10 (barkless) 4/10 (barkful)</li>
+                    <p>why is he barking bro</p>
+                    <p>the part where he's singing is very good and the beat is melodious</p>
+                    <li>"Excavator": 9.5/10</li>
+                    <p>lowkey goated song</p>
+                    <p>no explanation about this bro it's like the best</p>
+                    <li>"Gemstone": 9.7/10</li>
+                    <p>TRUMPETSSSSSS this is like OCTANE's "Tore Up"</p>
+                    <p>FAHHH</p>
+                    <li>"OPPOSITE": 8.9/10</li>
+                    <p>his flow is impeccable in this song</p>
+                    <li>"TMU": 4.0/10</li>
+                    <p>ts like lofi music bro and the transition from OPPOSITE to this was weird</p>
+                    <p>the chorus is also very repetitive</p>
+                    <li>"Pleasure's Mine": 6.0/10</li>
+                    <p>the production on this song is good</p>
+                    <li>"Sweet Home": 7.3/10</li>
+                    <p>def not a hype song but the lyrics are nice, especially good song to end an album with</p>
+                    <p>the beat is underwhelming, but the chorus is good</p>
                 </ol>
                 <h4>Our "Underrated" Bick:</h4>
               <div style={{
@@ -38,9 +61,9 @@ function DonnyBoyContent() {
                 borderRadius: 0,
                 padding: 0,
                 border: "1px solid #FCF5E5"
-              }}>
+              }}> // https://open.spotify.com/track/
                 <iframe 
-                src="https://open.spotify.com/embed/track/3C5yxZTCcDOi4pnFEnKJdC?si=ba4a479288fa4bbd?utm_source=generator&theme=0"
+                src="https://open.spotify.com/embed/track/3Hb28pse82bhnmW9sjTahL?si=044d603c854a43c7?si=ba4a479288fa4bbd?utm_source=generator&theme=0"
                   width="100%"
                   height="152"
                   
@@ -63,8 +86,8 @@ function DonnyBoyContent() {
                 border: "1px solid #FCF5E5"
               }}>
                 <iframe 
-                //https://open.spotify.com/track/03JZr55j9bIj1d2RQJ7Yq9?si=6a35cf4d0f6e491a
-                src="https://open.spotify.com/embed/track/03JZr55j9bIj1d2RQJ7Yq9?si=6a35cf4d0f6e491a?utm_source=generator&theme=0"
+                //https://open.spotify.com/track/6d9Cs2D2RSIl9c0ulgnFe8?si=3c25b31e3a0c4e17
+                src="https://open.spotify.com/embed/track/6d9Cs2D2RSIl9c0ulgnFe8?si=3c25b31e3a0c4e17?si=6a35cf4d0f6e491a?utm_source=generator&theme=0"
                   width="100%"
                   height="152"
                   
